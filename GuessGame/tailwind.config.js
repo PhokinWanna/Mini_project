050f0db80fac4039181da4a2fa36f73da942f81e
PhokinWanna <PETCHGRAMMER1@gmail.com>
2024-12-05 16:02:03 +0700

    MiniProject3

--- GuessGame/tailwind.config.js
+++ GuessGame/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
