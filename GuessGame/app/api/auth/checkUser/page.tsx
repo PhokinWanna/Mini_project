e88677d40a8119efadebd8bc743163f307a504db
PhokinWanna <PETCHGRAMMER1@gmail.com>
2024-12-08 18:27:08 +0700

    Final ded💀

--- GuessGame/app/api/auth/checkUser/page.tsx
+++ GuessGame/app/api/auth/checkUser/page.tsx
@@ -1,14 +1,14 @@
-import { getSession } from "@/utils/loginUser";
-import { redirect } from "next/navigation";
+// import { getSession } from "@/utils/loginUser";
+// import { redirect } from "next/navigation";
 
-export default async function checkUser() {
-  const user = await getSession();
+// export default async function checkUser() {
+//   const user = await getSession();
 
-  if (user) {
-    redirect("/game"); // If logged in, go to the game page
-  } else {
-    redirect("/api/auth/login"); // If not logged in, go to login page
-  }
+//   if (user) {
+//     redirect("/game"); // If logged in, go to the game page
+//   } else {
+//     redirect("/api/auth/login"); // If not logged in, go to login page
+//   }
 
-  return null;
-}
+//   return null;
+// }
